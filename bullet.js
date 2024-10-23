139b9911ea1e938669fe290700366fdc0f5354ac
Jacob <91891584+Asendant@users.noreply.github.com>
2024-10-23 08:42:40 -0700

    Added a bullet class and completed the constructor

---
+++ bullet.js
@@ -0,0 +1,10 @@
+class Bullet {
+  constructor(sizeX, sizeY, vectorX, vectorY, originX, originY) {
+    this.sizeX = sizeX;
+    this.sizeY = sizeY;
+    this.vectorX = vectorX;
+    this.vectorY = vectorY;
+    this.xPos = originX;
+    this.yPos = originY;
+  }
+}
